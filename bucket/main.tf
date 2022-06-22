f1baf75d4dd65df6a3c575383f2afae02aaf14e8
Fernando Lopes <lgbasilio@hotmail.com>
2022-06-22 11:57:16 -0300

    teste bucket

---
+++ bucket/main.tf
@@ -0,0 +1,9 @@
+resource google_storage_bucket buckettfstat {
+  name          = tfstate-loyal-road-353919-tfstate
+  force_destroy = false
+  location      = US
+  storage_class = STANDARD
+  versioning {
+    enabled = true
+      }
+}
