e6b55a7d803cb3b512225cb9cce3d8e9f36c55fa
Fernando Lopes <lgbasilio@hotmail.com>
2022-06-22 11:32:09 -0300

    teste

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+
+resource "google_storage_bucket" "buckettfstat" {
+  name          = "tfstate-loyal-road-353919-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+      }
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -17,13 +28,4 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-resource "google_storage_bucket" "buckettfstat" {
-  name          = "tfstate-loyal-road-353919-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-      }
-}
 
