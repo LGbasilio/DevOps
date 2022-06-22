15b730677f636148cf58112ca5b6eb6ba88888d9
Fernando Lopes <lgbasilio@hotmail.com>
2022-06-22 11:16:01 -0300

    tfstat

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "qwiklabs-gcp-00-90b1203a84f4"
     prefix  = "terraform/state"
-	depends_on = [google_storage_bucket.bucket_buckettfstat]
-  }
+	  }
+depends_on = [google_storage_bucket.bucket_buckettfstat]
 }
