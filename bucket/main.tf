9029dd7a7d60c4ee19354ca1defeda2c8d3d0cec
Fernando Lopes <lgbasilio@hotmail.com>
2022-06-22 12:23:24 -0300

    aagora vai

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "buckettfstat" {
-  name          = tfstate-loyal-road-353919-tfstate
+  name          = "tfstate-loyal-road-353919-tfstate"
   force_destroy = false
-  location      = US
-  storage_class = STANDARD
+  location      = "US"
+  storage_class = "STANDARD"
   versioning {
     enabled = true
       }
