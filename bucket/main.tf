7260b8558a11c7763802b1ec1fd5b6916b88f9d4
Fernando Lopes <lgbasilio@hotmail.com>
2022-06-22 12:20:41 -0300

    final, eu acho

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,4 +1,4 @@
-resource google_storage_bucket buckettfstat {
+resource "google_storage_bucket" "buckettfstat" {
   name          = tfstate-loyal-road-353919-tfstate
   force_destroy = false
   location      = US
